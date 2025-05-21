141b780e42cea446bf854f31906efc9e843b069a
shannonhickman2004 <shannonhickman2004@gmail.com>
2025-05-21 13:27:46 -0600

    week3-practice

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute");
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates 
@@ -24,9 +26,23 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" });
-});
+app.get("/", baseController.buildHome);
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
