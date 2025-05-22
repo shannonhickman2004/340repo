f1a4e42a75868686da08ad36cf1508992ad36814
shannonhickman2004 <shannonhickman2004@gmail.com>
2025-05-21 18:58:11 -0600

    week3try

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
